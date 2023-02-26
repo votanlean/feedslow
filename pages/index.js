3d3340718c3a831b87c0fc5f8ac346dfe937866c
Alex Vo <votanlean@gmail.com>
2023-02-26 16:31:02 +0700

    Init

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HomePage = () => (
+  <div className="flex h-screen items-center justify-center">
+    <span class=" bg-sky-200">Hello, World!</span>
+  </div>
+);
+
+export default HomePage;
